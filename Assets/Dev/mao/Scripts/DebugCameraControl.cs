38316de5885a41daebde12d0000a343d68581de5
mao <miymao78.d@gmail.com>
2016-08-27 18:18:00 +0900

    テレポートの仮処理一式を新規追加

---
+++ Assets/Dev/mao/Scripts/DebugCameraControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugCameraControl : MonoBehaviour
+{
+#if UNITY_EDITOR
+    private float horizontal, vertical;
+
+    Quaternion rotation;
+
+    void Start()
+    {
+        rotation = transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        horizontal += Input.GetAxis("Horizontal");
+        vertical += Input.GetAxis("Vertical");
+
+        var rot = rotation;
+        rot *= Quaternion.AngleAxis(horizontal, Vector3.up);
+        rot *= Quaternion.AngleAxis(vertical, Vector3.right);
+        transform.rotation = rot;
+    }
+#endif
+}
