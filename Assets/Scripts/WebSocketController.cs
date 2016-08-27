b8f8051c8a0fb8a56321e30fee2c6bc808519f43
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-08-27 21:17:51 +0900

    WebSocketでリアルタイム通信できるようにとりあえずしておきました

---
+++ Assets/Scripts/WebSocketController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+public class WebSocketController : SingletonBehaviour<WebSocketController> {
+	WebSocket ws;
+	private const string WSAddress = "ws://192.168.220.49:3000";
+
+	void Start(){
+		Connect ();
+	}
+
+	void Connect () {
+		ws = new WebSocket (WSAddress);
+
+		ws.OnOpen += (sender, e) => {
+			Debug.Log ("WebSocket Open");
+		};
+
+		ws.OnMessage += (sender, e) => {
+			Debug.Log ("WebSocket Message Type: " + e.Type + ", Data: " + e.Data);
+		};
+
+		ws.OnError += (sender, e) => {
+			Debug.Log ("WebSocket Error Message: " + e.Message);
+		};
+
+		ws.OnClose += (sender, e) => {
+			Debug.Log ("WebSocket Close");
+		};
+
+		ws.Connect ();
+	}
+
+	void Disconnect () {
+		ws.Close ();
+		ws = null;
+	}
+
+	void OnDestroy () {
+		Disconnect ();
+	}
+}
