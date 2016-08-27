71e3fa97c8e717b9ef375de6e9dae3f24f90b74d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-08-27 21:53:01 +0900

    WebSocketControllerが使いやすくなるように実装しました

--- Assets/Scripts/WebSocketController.cs
+++ Assets/Scripts/WebSocketController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 using WebSocketSharp;
 using WebSocketSharp.Net;
@@ -7,12 +9,33 @@ using WebSocketSharp.Net;
 public class WebSocketController : SingletonBehaviour<WebSocketController> {
 	WebSocket ws;
 	private const string WSAddress = "ws://192.168.220.49:3000";
+	private List<string> receivedMessageList = new List<string>();
+	private Dictionary<GameObject, Action<string>> callbackList = new Dictionary<GameObject, Action<string>>();
 
-	void Start(){
-		Connect ();
+	public override void SingleAwake() {
+		Connect();
 	}
 
-	void Connect () {
+	public void AddMessageReceiveCallback(GameObject obj, Action<string> callback){
+		if (callbackList.ContainsKey (obj)) return;
+		callbackList.Add(obj, callback);
+	}
+
+	public void RemoveMessageReceiveCallback(GameObject obj){
+		if (!callbackList.ContainsKey (obj)) return;
+		callbackList.Remove(obj);
+	}
+
+	void Update(){
+		for (int i = 0; i < receivedMessageList.Count; ++i) {
+			foreach (var callback in callbackList) {
+				callback.Value(receivedMessageList[i]);
+			}
+		}
+		receivedMessageList.Clear();
+	}
+
+	public void Connect () {
 		ws = new WebSocket (WSAddress);
 
 		ws.OnOpen += (sender, e) => {
@@ -21,6 +44,9 @@ public class WebSocketController : SingletonBehaviour<WebSocketController> {
 
 		ws.OnMessage += (sender, e) => {
 			Debug.Log ("WebSocket Message Type: " + e.Type + ", Data: " + e.Data);
+			lock(receivedMessageList){
+				receivedMessageList.Add(e.Data);
+			}
 		};
 
 		ws.OnError += (sender, e) => {
@@ -34,7 +60,7 @@ public class WebSocketController : SingletonBehaviour<WebSocketController> {
 		ws.Connect ();
 	}
 
-	void Disconnect () {
+	public void Disconnect () {
 		ws.Close ();
 		ws = null;
 	}
