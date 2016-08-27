7795577d44edb0973cb65fc849a445f39f67d3f1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-08-27 20:53:20 +0900

    サーバー側のwebsocketの処理も入れちゃう

---
+++ server/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+//サーバーの立ち上げ
+var http = require('http');
+
+//指定したポートにきたリクエストを受け取れるようにする
+var server = http.createServer(app).listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({server:server});
+
+var connections = []; 
+wss.on('connection', function (ws) {
+  console.log('connect!!');
+  connections.push(ws);
+  ws.on('close', function () {
+    console.log('close');
+    connections = connections.filter(function (conn, i) {
+      return (conn === ws) ? false : true;
+    });
+  });
+  ws.on('message', function (message) {
+    console.log('message:', message);
+    connections.forEach(function (con, i) {
+      con.send(message);
+    });
+  });
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
