10c3d884177459ad6708feac4f224f0d6f33aa84
Makoto__Adachi <adatu@hotmail.co.jp>
2016-08-28 16:59:02 +0900

    BGMを追加

---
+++ Assets/Scripts/MainBGM.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainBGM : MonoBehaviour {
+	private AudioClip audioSE = null;
+	private AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play ();
+	}
+}
