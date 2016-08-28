7ab05e26e29473268f77bacad958ca8ecc451db2
Hikaru Suido <light1tus@gmail.com>
2016-08-28 14:30:53 +0900

    リザルト画面の調整

---
+++ Assets/To_Title_Scene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class To_Title_Scene : MonoBehaviour {
+
+    public void ToTitle()
+    {
+        Application.LoadLevel("Title");
+    }
+}
